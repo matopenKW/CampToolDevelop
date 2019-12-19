497322aaad9bc05c98c3d9265deec675885a6eb4
小野和論 <matopenkw@gmail.com>
2019-12-19 17:24:56 +0900

    Update kotsuhi.go 登録処理の実装

--- internal/apps/kotsuhi.go
+++ internal/apps/kotsuhi.go
@@ -16,7 +16,7 @@ type Kotsuhi struct {
 	End          string
 	Start        string
 	RoundTripFlg int64
-	Price        int64
+	Price        string
 }
 
 func ExeKotsuhi(req *http.Request, client *firestore.Client) (gin.H, error) {
@@ -52,12 +52,13 @@ func view(req *http.Request, client *firestore.Client) (gin.H, error) {
 		}
 
 		data := doc.Data()
+		log.Println(data)
 
 		kotsuhi := &Kotsuhi{
-			data["end"].(string),
-			data["start"].(string),
-			data["round_trip_flg"].(int64),
-			data["price"].(int64),
+			isNotNil(data["End"]),
+			isNotNil(data["Start"]),
+			isNotNilNum(data["roundTripFlg"]),
+			isNotNil(data["Price"]),
 		}
 
 		list = append(list, kotsuhi)
@@ -71,5 +72,94 @@ func view(req *http.Request, client *firestore.Client) (gin.H, error) {
 
 func regist(req *http.Request, client *firestore.Client) (gin.H, error) {
 
+	req.ParseForm()
+
+	rowSize := len(req.Form["start"])
+
+	_, err := client.Collection("tomoki").Doc("DC").Delete(context.Background())
+	if err != nil {
+		// Handle any errors in an appropriate way, such as returning them.
+		log.Printf("An error has occurred: %s", err)
+	}
+
+	for i := 0; i < rowSize; i++ {
+		log.Printf("start:%v %T/n", req.Form["start"][i], req.Form["start"][i])
+		log.Printf("end:%v %T/n", req.Form["end"][i], req.Form["end"][i])
+		log.Printf("plice:%v %T/n", req.Form["plice"][i], req.Form["plice"][i])
+		log.Printf("bikou:%v %T/n", req.Form["bikou"][i], req.Form["bikou"][i])
+
+		kotsuhi := &Kotsuhi{
+			req.Form["end"][i],
+			req.Form["start"][i],
+			//req.Form["round_trip_flg"][i].(int64),
+			0,
+			req.Form["plice"][i],
+		}
+
+		ref := client.Collection("tomoki").NewDoc()
+		_, err := ref.Set(context.Background(), kotsuhi)
+
+		//		_, _, err := client.Collection("tomoki").Add(context.Background(), kotsuhi)
+
+		if err != nil {
+			return nil, err
+		}
+
+	}
+
 	return view(req, client)
 }
+
+func isNotNil(obj interface{}) string {
+	if obj != nil {
+		return obj.(string)
+	} else {
+		return ""
+	}
+}
+
+func isNotNilNum(obj interface{}) int64 {
+	if obj != nil {
+		return obj.(int64)
+	} else {
+		return 0
+	}
+}
+
+// func deleteCollection(ctx context.Context, client *firestore.Client,
+// 	ref *firestore.CollectionRef, batchSize int) error {
+
+// 	for {
+// 		// Get a batch of documents
+// 		iter := ref.Limit(batchSize).Documents(ctx)
+// 		numDeleted := 0
+
+// 		// Iterate through the documents, adding
+// 		// a delete operation for each one to a
+// 		// WriteBatch.
+// 		batch := client.Batch()
+// 		for {
+// 			doc, err := iter.Next()
+// 			if err == iterator.Done {
+// 				break
+// 			}
+// 			if err != nil {
+// 				return err
+// 			}
+
+// 			batch.Delete(doc.Ref)
+// 			numDeleted++
+// 		}
+
+// 		// If there are no documents to delete,
+// 		// the process is over.
+// 		if numDeleted == 0 {
+// 			return nil
+// 		}
+
+// 		_, err := batch.Commit(ctx)
+// 		if err != nil {
+// 			return err
+// 		}
+// 	}
+// }
