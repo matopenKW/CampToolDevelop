163575d951fd8f85dcbd0c136e7cf0692af9cc8a
小野和論 <matopenkw@gmail.com>
2019-12-19 23:53:04 +0900

    Update firebase.go コレクション削除処理の追加

--- pkg/db/firebase.go
+++ pkg/db/firebase.go
@@ -6,6 +6,8 @@ import (
 	"cloud.google.com/go/firestore"
 	firebase "firebase.google.com/go"
 	"google.golang.org/api/option"
+
+	"google.golang.org/api/iterator"
 )
 
 const keyjson = "pkg/conf/key.json"
@@ -23,4 +25,42 @@ func OpenFirebase() (*firestore.Client, error) {
 	}
 
 	return client, nil
+}
+
+func DeleteCollection(ctx context.Context, client *firestore.Client,
+	ref *firestore.CollectionRef, batchSize int) error {
+
+	for {
+		// Get a batch of documents
+		iter := ref.Limit(batchSize).Documents(ctx)
+		numDeleted := 0
+
+		// Iterate through the documents, adding
+		// a delete operation for each one to a
+		// WriteBatch.
+		batch := client.Batch()
+		for {
+			doc, err := iter.Next()
+			if err == iterator.Done {
+				break
+			}
+			if err != nil {
+				return err
+			}
+
+			batch.Delete(doc.Ref)
+			numDeleted++
+		}
+
+		// If there are no documents to delete,
+		// the process is over.
+		if numDeleted == 0 {
+			return nil
+		}
+
+		_, err := batch.Commit(ctx)
+		if err != nil {
+			return err
+		}
+	}
 }
