31dee486b8a550cfe5cdd9df5990ea90ae59c73f
小野和論 <matopenkw@gmail.com>
2019-12-19 17:24:02 +0900

    Update main.go  templateパスを設定する

--- main.go
+++ main.go
@@ -11,7 +11,25 @@ import (
 	"log"
 
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
 )
+
+var templatePathMap map[string]string
+
+func init() {
+
+	templatePathMap = make(map[string]string)
+	templateDir := "templates/"
+
+	files, err := ioutil.ReadDir(templateDir)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, file := range files {
+		templatePathMap[util.SubstrBefore(file.Name(), ".")] = templateDir + file.Name()
+	}
+}
 
 func main() {
 
@@ -27,35 +45,28 @@ func main() {
 	}
 	defer client.Close()
 
-	router.POST("/", htmlForward(router, client))
-	router.GET("/", htmlForward(router, client))
+	router.POST("/", htmlForward(router, client, templatePathMap["index"]))
+	router.GET("/", htmlForward(router, client, templatePathMap["index"]))
 
-	router.POST("/index", htmlForward(router, client))
-	router.GET("/index", htmlForward(router, client))
+	router.POST("/index", htmlForward(router, client, templatePathMap["index"]))
+	router.GET("/index", htmlForward(router, client, templatePathMap["index"]))
 
-	router.POST("kotsuhi:regist", htmlForward(router, client))
-	router.GET("/kotsuhi", htmlForward(router, client))
+	router.GET("/kotsuhi", htmlForward(router, client, templatePathMap["kotsuhi"]))
+	router.POST("kotsuhi:regist", htmlForward(router, client, templatePathMap["kotsuhi"]))
 
 	router.Run()
 }
 
-func htmlForward(router *gin.Engine, client *firestore.Client) func(ctx *gin.Context) {
+func htmlForward(router *gin.Engine, client *firestore.Client, templatePath string) func(ctx *gin.Context) {
 	return func(ctx *gin.Context) {
-		path := ctx.Request.URL.Path
 
-		htmlPath := util.SubstrBefore(path, ":")
-		log.Println(htmlPath)
-		if htmlPath == "/" {
-			htmlPath = htmlPath + "index"
-		}
-
-		actionPath := util.SubstrAfter(path, "/")
-
-		html := template.Must(template.ParseFiles("templates"+htmlPath+".html", "templates/base.html"))
+		html := template.Must(template.ParseFiles(templatePath, templatePathMap["base"]))
 		router.SetHTMLTemplate(html)
 
 		var err error
 		form := gin.H{}
+
+		actionPath := util.SubstrBefore(util.SubstrAfter(ctx.Request.URL.Path, "/"), ":")
 
 		switch actionPath {
 		case "", "index":
@@ -68,7 +79,6 @@ func htmlForward(router *gin.Engine, client *firestore.Client) func(ctx *gin.Con
 		if err != nil {
 			log.Fatalf("erro in new db client. reason : %v\n", err)
 		}
-
 		ctx.HTML(200, "base.html", form)
 	}
 }
