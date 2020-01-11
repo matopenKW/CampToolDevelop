76566890bd404bb25be475cd7912570b29c59444
小野和論 <matopenkw@gmail.com>
2020-01-11 16:22:15 +0900

    Update main.go エラーハンドリングの追加

--- internal/main.go
+++ internal/main.go
@@ -51,7 +51,10 @@ func main() {
 	router.POST("/", htmlForward(router, client, templatePathMap["login"]))
 	router.GET("/login", htmlForward(router, client, templatePathMap["login"]))
 	router.POST("/login", htmlForward(router, client, templatePathMap["login"]))
-	router.POST("/login:login", htmlForward(router, client, templatePathMap["index"]))
+
+	// Ajaxでチェックをかましてからサブミット→サブミット時にサイドチェックで　クッキー作成とHTMLフォワード
+	router.POST("/login:chkLogin", jsonForward(client))
+	//router.POST("/login:login", login(router, client))
 
 	// router.GET("/index", htmlForward(router, client, templatePathMap["index"]))
 	// router.POST("/index", htmlForward(router, client, templatePathMap["index"]))
@@ -67,9 +70,6 @@ func main() {
 
 func htmlForward(router *gin.Engine, client *firestore.Client, templatePath string) func(ctx *gin.Context) {
 	return func(ctx *gin.Context) {
-
-		html := template.Must(template.ParseFiles(templatePath, templatePathMap["base"]))
-		router.SetHTMLTemplate(html)
 
 		var err error
 		form := map[string]interface{}{}
@@ -87,27 +87,61 @@ func htmlForward(router *gin.Engine, client *firestore.Client, templatePath stri
 		}
 
 		if err != nil {
-			log.Fatalf("error : %v\n", err)
+			log.Printf("error : %v\n", err)
+			code, errForm := errorHandring(err)
+			ctx.HTML(code, "base.html", errForm)
+		} else {
+			html := template.Must(template.ParseFiles(templatePath, templatePathMap["base"]))
+			router.SetHTMLTemplate(html)
+
+			ctx.HTML(200, "base.html", form)
 		}
-		ctx.HTML(200, "base.html", form)
 	}
 }
 
 func jsonForward(client *firestore.Client) func(ctx *gin.Context) {
 	return func(ctx *gin.Context) {
 
-		form, err := apps.ExeCarfare(ctx.Request, client)
+		var err error
+		form := map[string]interface{}{}
 
-		if err != nil {
-			log.Fatalf("erro in new db client. reason : %v\n", err)
+		actionPath := util.SubstrBefore(util.SubstrAfter(ctx.Request.URL.Path, "/"), ":")
+
+		switch actionPath {
+		case "login":
+			form, err = apps.ExeLogin(ctx.Request, client)
+		case "index":
+
+		case "carfare":
+			form, err = apps.ExeCarfare(ctx.Request, client)
+		default:
 		}
 
-		jsonForm, err := json.Marshal(form)
-		if err != nil {
-			log.Fatalf("failed　to　json　convert : %v\n", err)
-		}
+		setJsonFunc := setJson(ctx)
 
-		log.Println(string(jsonForm))
-		ctx.JSON(200, string(jsonForm))
+		if err != nil {
+			log.Printf("failed　to　json　convert : %v\n", err)
+			setJsonFunc(errorHandring(err))
+		} else {
+			jsonForm, err := json.Marshal(form)
+			if err != nil {
+				setJsonFunc(errorHandring(err))
+			} else {
+				log.Println(string(jsonForm))
+				setJsonFunc(200, form)
+			}
+		}
+	}
+}
+
+func errorHandring(err error) (code int, form map[string]interface{}) {
+	return 500, map[string]interface{}{
+		"errMssage": "error : " + err.Error(),
+	}
+}
+
+func setJson(ctx *gin.Context) func(code int, obj interface{}) {
+	return func(code int, obj interface{}) {
+		ctx.JSON(code, obj)
 	}
 }
