44bc25074f0c570a87779a8fb3aab80f1389ce8d
小野和論 <matopenkw@gmail.com>
2020-01-13 14:52:45 +0900

    Update login.js エラーハンドリングの追加

--- web/assets/js/login.js
+++ web/assets/js/login.js
@@ -28,17 +28,7 @@ function aush(){
 	.then(function(user) {
 		login(user);
 	})
-	.catch(function(error) {
-		var errorCode = error.code;
-		var errorMessage = error.message;
-
-		if (errorCode === 'auth/wrong-password') {
-		  alert('Wrong password.');
-		} else {
-		  alert(errorMessage);
-		}
-		console.log(error);
-	  });
+	.catch(firebaseErrHandring);
 }
 
 function login(user) {
@@ -62,15 +52,33 @@ function createAccount(){
 		alert('ユーザーを新規登録しました。');
 		login(user);
 	})
-	.catch(function(error) {
-		var errorCode = error.code;
-		var errorMessage = error.message;
+	.catch(firebaseErrHandring);
+}
 
-		if (errorCode === "auth/email-already-in-use"){
-			alert('既に登録してあるメールアドレスです。');			
-		} else {
-			alert(errorMessage);
-		}
-		console.log(error);
-	});
+function firebaseErrHandring(error){
+	var errorCode = error.code;
+	var errorMessage = error.message;
+
+	if (errorCode === 'auth/invalid-email') {
+		alert('メールアドレスの形式が不正です。');
+
+	} else if (errorCode === 'auth/wrong-password') {
+		alert('パスワードが間違っている又は不正な形式です。');
+
+	} else if (errorCode === 'auth/user-not-found') {
+		alert('存在しないユーザー又は削除された可能性があります。');
+
+	} else if (errorCode === 'auth/email-already-in-use'){
+		alert('既に登録してあるメールアドレスです。');
+
+	} else if (errorCode === 'auth/weak-password') {
+		alert('パスワードは６桁以上で登録してください。');
+
+	} else {
+		  alert(errorMessage);
+
+	}
+
+	console.log(error);
+
 }
