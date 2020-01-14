9a8635a394663b87934c073522b4de632835b1e2
小野和論 <matopenkw@gmail.com>
2020-01-14 20:49:47 +0900

    Update print_cerfare.go エクセル出力処理のテスト実装

--- pkg/excel/print_cerfare.go
+++ pkg/excel/print_cerfare.go
@@ -2,44 +2,162 @@ package main
 
 import (
 	"CampToolDevelop/internal/apps"
-	"fmt"
+	_ "github.com/loadoff/excl"
+	"github.com/tealeg/xlsx"
+	"log"
+	"strings"
 	"time"
 
-	"github.com/loadoff/excl"
+	"fmt"
 )
 
 const EXCEL_PATH = "./work/"
 
+const TAG_1 = "$"
+const TAG_2 = "_"
+
+var positionMap = make(map[string]CellPosition)
+
 func main() {
 
-	wookbook, _ := excl.Open(EXCEL_PATH + "test1.xlsx")
-	defer wookbook.Close()
+	read1()
+	write()
 
-	sheet, _ := wookbook.OpenSheet("Sheet1")
-	defer sheet.Close()
-
-	// 1列目のセルを取得
-	// c := r.GetCell(1)
-	// // セルに10を出力
-	// c.SetNumber("10")
-	// // 2列目のセルにABCDEという文字列を出力
-	// c = r.SetString("ABCDE", 2)
-
-	startIndex := 5
-	for i, v := range selectDataList() {
-
-		row := sheet.GetRow(startIndex + i)
-
-		cell1 := row.GetCell(2)
-		cell1.SetDate(cnvDate(v.Date))
-
-		fmt.Println(v, i)
-
-	}
-
-	wookbook.Save(EXCEL_PATH + "new.xlsx")
 }
 
+type CellPosition struct {
+	key       string
+	rowIndex  int
+	cellIndex int
+}
+
+func (cellp *CellPosition) getPosition() (rowIdx int, cellIdx int) {
+	return cellp.rowIndex, cellp.cellIndex
+}
+
+func read1() {
+	file, err := xlsx.OpenFile(EXCEL_PATH + "test1.xlsx")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, sheet := range file.Sheets {
+		for rowIdx, row := range sheet.Rows {
+			for cellIdx, cell := range row.Cells {
+				v := cell.String()
+
+				if strings.Index(v, TAG_1) > -1 {
+					cellp := CellPosition{
+						key:       v,
+						rowIndex:  rowIdx,
+						cellIndex: cellIdx,
+					}
+
+					positionMap[v] = cellp
+
+				}
+
+			}
+		}
+	}
+}
+
+func write() {
+
+	file, err := xlsx.OpenFile(EXCEL_PATH + "test1.xlsx")
+	if err != nil {
+		log.Fatal(err)
+	}
+	sheet := file.Sheets[0]
+
+	print := getDataMap()
+
+	log.Println(positionMap)
+
+	for k, v := range print {
+
+		cellp := positionMap["${"+k+"}"]
+
+		sheet.Cell(cellp.getPosition()).Value = v
+	}
+
+	err = file.Save(EXCEL_PATH + "new.xlsx")
+	if err != nil {
+		log.Printf(err.Error())
+	}
+
+	log.Println("success")
+}
+
+// func read2() {
+
+// 	wookbook, _ := excl.Open(EXCEL_PATH + "test1.xlsx")
+// 	defer wookbook.Close()
+
+// 	sheet, _ := wookbook.OpenSheet("Sheet1")
+// 	defer sheet.Close()
+
+// 	i := 0
+// 	for i < 5 {
+// 		row := sheet.GetRow(i)
+
+// 		cells := row.CreateCells(1, 5)
+
+// 		for _, v := range cells {
+
+// 			log.Println(v)
+
+// 		}
+
+// 		// cell1 := row.GetCell(2)
+
+// 		i++
+// 	}
+
+// }
+
+// func print() {
+
+// 	wookbook, _ := excl.Open(EXCEL_PATH + "test1.xlsx")
+// 	defer wookbook.Close()
+
+// 	sheet, _ := wookbook.OpenSheet("Sheet1")
+// 	defer sheet.Close()
+
+// 	startIndex := 5
+// 	for i, v := range selectDataList() {
+
+// 		row := sheet.GetRow(startIndex + i)
+
+// 		cell1 := row.GetCell(2)
+// 		cell1.SetDate(cnvDate(v.Date))
+
+// 		log.Println(v, i)
+
+// 	}
+
+// 	wookbook.Save(EXCEL_PATH + "new.xlsx")
+// }
+
+func getDataMap() map[string]string {
+
+	ret := make(map[string]string)
+
+	ret["name"] = "Kazurnoti Ono"
+
+	list := selectDataList()
+
+	for i, v := range list {
+		idx := i + 1
+		ret[fmt.Sprintf("IKI_%d", idx)] = v.Start
+		ret[fmt.Sprintf("kaeri_%d", idx)] = v.End
+		ret[fmt.Sprintf("bikou_%d", idx)] = v.Bikou
+	}
+
+	log.Println(ret)
+
+	return ret
+}
 func selectDataList() []*apps.Carfare {
 	list := make([]*apps.Carfare, 0, 10)
 	carfare := &apps.Carfare{Date: "20200113", Start: "町田", End: "新宿", Price: "2000", Bikou: "テスト"}
